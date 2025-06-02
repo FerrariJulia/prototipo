e8cc46e1fae005b97ded57393624f3c0be47a803
FerrariJulia <167891106+FerrariJulia@users.noreply.github.com>
2025-06-02 17:57:57 -0300

    subida prototipo

---
+++ prototipo/prototipo.js
@@ -0,0 +1,55 @@
+/*
+TP1-Etapa 2; Prototipo.
+ Julia Ferrari, Michelle González,
+ Micaela Floch, Lucas Gordillo,
+ Pilar Fernández, Martina Furh
+ Comisión Matías.
+ */
+
+let trazos = [];
+let cantidad = 4;
+let cant = 20; // cantidad de filas y columnas
+let size = 40;
+let espaciadoX = 50; // espacio horizontal
+let espaciadoY = 5; // espacio vertical
+let imgWidth = 60;  // angosto
+let imgHeight = 20; // alto
+let avance = 0;
+let vel = 1;
+//let estado;
+
+function preload() {
+  for (let i = 0; i < cantidad; i++) {
+    let ufan = "data/trazo" + nf(i, 2) + ".png";
+    trazos[i] = loadImage(ufan);
+  }
+}
+
+function setup() {
+  createCanvas(800, 800);
+  //imageMode(CENTER);
+  //colorMode(HSB, 360, 100, 100);
+  background(255);
+}
+
+function draw() {
+
+ // if (estado === "inicial") {
+    avance += vel;
+    if (avance > imgWidth + espaciadoX) {
+      avance = 0;
+    }
+
+    for (let i = 0; i < cant; i++) {
+      for (let g = 0; g < cant; g++) {
+        // filas impares/pares
+        let desplazamientoX = (i % 2 === 1) ? imgWidth / 2 : 0;
+
+        let x = g * (imgWidth + espaciadoX) + imgWidth / 2 + desplazamientoX - avance;
+        let y = i * (imgHeight + espaciadoY) + imgHeight / 2;
+
+        let img = trazos[0]; 
+        image(img, x, y, imgWidth, imgHeight);
+      }
+    }
+  }
